8cb250a0ea4bb6268b73c21b0465e0c0f51ef5ef
JoshHambright <josh.hambright@gmail.com>
2020-10-26 10:03:58 -0400

    Add project files.

---
+++ TriviaGame/Questions/TriviaQuestion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGame.Questions
+{
+    public enum TriviaCategory
+    {
+        General,
+        PopCulture,
+        Science,
+        History,
+        Geography
+    }
+
+    public class TriviaQuestion
+    {
+        public int QuestionID { get; set; }
+
+        public string Question { get; set; }
+
+        public string CorrectAnswer { get; set; }
+        public string WrongAnswer1 { get; set; }
+        public string WrongAnswer2 { get; set; }
+        public string WrongAnswer3 { get; set; }
+
+        public TriviaCategory TriviaCategory { get; set; }
+        
+        public TriviaQuestion() { }
+
+        public TriviaQuestion(int questionID, string question, string correctAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3, TriviaCategory category)
+        {
+            QuestionID = questionID;
+            Question = question;
+            CorrectAnswer = correctAnswer;
+            WrongAnswer1 = wrongAnswer1;
+            WrongAnswer2 = wrongAnswer2;
+            WrongAnswer3 = wrongAnswer3;
+            TriviaCategory = category;
+        }
+
+    }
+
+
+
+
+}
+
---
+++ TriviaGame/Questions/TriviaQuestion_Repo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGame.Questions
+{
+    public class TriviaQuestion_Repo
+    {
+        private List<TriviaQuestion> _questions = new List<TriviaQuestion>();
+
+
+        //Create
+        public bool AddQuestionToList(TriviaQuestion question)
+        {
+            int StartingCount = _questions.Count;
+
+            _questions.Add(question);
+            bool wasAdded = (_questions.Count > StartingCount) ? true : false;
+            return wasAdded;
+        }
+
+        // Read
+        public List<TriviaQuestion> GetQuestions()
+        {
+            return _questions;
+        }
+
+        public TriviaQuestion GetQuestionById(int id)
+        {
+            foreach (TriviaQuestion question in _questions)
+            {
+                if(question.QuestionID == id)
+                {
+                    return question;
+                }
+            }
+            return null;
+        }
+
+        // Update
+        public bool UpdateExistingQuestion(int id, TriviaQuestion newQuestion)
+        {
+            TriviaQuestion oldQuestion = GetQuestionById(int id);
+            if (oldQuestion != null)
+            {
+
+                oldQuestion.QuestionID = newQuestion.questionID;
+                oldQuestion.Question = newQuestion.question;
+                oldQuestion.CorrectAnswer = correctAnswer;
+                WrongAnswer1 = wrongAnswer1;
+                WrongAnswer2 = wrongAnswer2;
+                WrongAnswer3 = wrongAnswer3;
+                TriviaCategory = category;
+            }
+
+
+
+
+
+
+        // Delete
+    }
+}
